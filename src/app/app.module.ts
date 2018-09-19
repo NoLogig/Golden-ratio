36eee74438eef4ebb813803c1684660e8fdfc5a2
NoLogig <max-schweigerdt@web.de>
2018-09-20 00:03:01 +0200

    add/setup @angular/material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,26 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatTabsModule, MatCheckboxModule,
+MatButtonToggleModule, MatExpansionModule, MatFormFieldModule, MatDatepickerModule,
+MatNativeDateModule, MatSlideToggleModule, MatSortModule, MatSidenavModule, MatProgressBarModule,
+MatSelectModule, MatCardModule, MatInputModule, MatButtonModule, MatListModule, MatIconModule, MatSliderModule } from '@angular/material';
+import 'hammerjs';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    BrowserAnimationsModule, MatInputModule, MatButtonModule, MatIconModule, MatListModule, MatCardModule, LayoutModule, MatSliderModule,
+    MatToolbarModule, MatTabsModule, MatCheckboxModule, MatButtonToggleModule, MatExpansionModule, MatFormFieldModule, MatDatepickerModule,
+    MatNativeDateModule, MatSlideToggleModule, MatSortModule, MatSidenavModule, MatSelectModule, MatProgressBarModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
