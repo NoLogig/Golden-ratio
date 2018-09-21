ea3d82e82a552dc90df78e137b32767daea3192d
NoLogig <max-schweigerdt@web.de>
2018-09-21 11:46:17 +0200

    SCSS integration / Material theming

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule, MatTabsModule, MatCheckboxModule,
-MatButtonToggleModule, MatExpansionModule, MatFormFieldModule, MatDatepickerModule,
-MatNativeDateModule, MatSlideToggleModule, MatSortModule, MatSidenavModule, MatProgressBarModule,
-MatSelectModule, MatCardModule, MatInputModule, MatButtonModule, MatListModule, MatIconModule, MatSliderModule } from '@angular/material';
+import {
+  MatIconModule, MatButtonModule, MatCardModule, MatInputModule, MatToolbarModule
+} from '@angular/material';
 import 'hammerjs';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GoldenRatioComponent } from './golden-ratio/golden-ratio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GoldenRatioComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-
-    BrowserAnimationsModule, MatInputModule, MatButtonModule, MatIconModule, MatListModule, MatCardModule, LayoutModule, MatSliderModule,
-    MatToolbarModule, MatTabsModule, MatCheckboxModule, MatButtonToggleModule, MatExpansionModule, MatFormFieldModule, MatDatepickerModule,
-    MatNativeDateModule, MatSlideToggleModule, MatSortModule, MatSidenavModule, MatSelectModule, MatProgressBarModule,
-
+    BrowserAnimationsModule,
+    LayoutModule, MatIconModule,
+    MatButtonModule, MatCardModule, MatInputModule, MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/golden-ratio/golden-ratio.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-golden-ratio',
+  templateUrl: './golden-ratio.component.html',
+  styleUrls: ['./golden-ratio.component.scss']
+})
+export class GoldenRatioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
