017789a8125f4cf500463260cf262a6fd46e15ec
NoLogig <max-schweigerdt@web.de>
2018-10-02 23:56:17 +0200

    Golden ratio TS val generator

--- src/app/golden-ratio/golden-ratio.component.ts
+++ src/app/golden-ratio/golden-ratio.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+interface IGoldenRatio {
+  main: number;
+  major: number;
+  minor: number;
+}
+
 @Component({
   selector: 'app-golden-ratio',
   templateUrl: './golden-ratio.component.html',
@@ -7,10 +13,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GoldenRatioComponent implements OnInit {
 
+  private goldenRatio = 1.61803398875;
   public scaleView: number;
 
   constructor() { }
 
   ngOnInit() { }
+
+  goldenMain(main: number): IGoldenRatio | void {
+    // if (isNaN(main)) { return throwParaError(); }
+
+    let major = main / this.goldenRatio;
+    let minor = main - major;
+
+    return { main, major, minor };
+  }
+
+  goldenMajor(major: number): IGoldenRatio | void {
+    // if (isNaN(major)) { return throwParaError(); }
+
+    let main = major * this.goldenRatio;
+    let minor = main - major;
+
+    return { main, major, minor };
+  }
+
+  goldenMinor(minor: number): IGoldenRatio | void {
+    // if (isNaN(minor)) { return throwParaError(); }
+
+    let major = minor * this.goldenRatio;
+    let main = minor + major;
+
+    return { main, major, minor, };
+  }
 
 }
