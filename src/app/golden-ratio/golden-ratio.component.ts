ea1514a832464cbc4d0d187ec9ddfad25116924c
NoLogig <max-schweigerdt@web.de>
2018-10-03 09:58:45 +0200

    minor changes

--- src/app/golden-ratio/golden-ratio.component.ts
+++ src/app/golden-ratio/golden-ratio.component.ts
@@ -14,33 +14,40 @@ interface IGoldenRatio {
 export class GoldenRatioComponent implements OnInit {
 
   private goldenRatio = 1.61803398875;
+
   public scaleView: number;
 
-  constructor() { }
+  constructor() {
+
+    console.log('Main:  %o', this.goldenMain(100));
+    console.log('Major: %o', this.goldenMajor(100));
+    console.log('Minor: %o', this.goldenMinor(100));
+
+  }
 
   ngOnInit() { }
 
-  goldenMain(main: number): IGoldenRatio | void {
+  //  @param main Represents the "main" segment
+  goldenMain(main: number): IGoldenRatio { // | void {
     // if (isNaN(main)) { return throwParaError(); }
-
     let major = main / this.goldenRatio;
     let minor = main - major;
 
     return { main, major, minor };
   }
 
-  goldenMajor(major: number): IGoldenRatio | void {
+  //  @param major Represents the "major" segment
+  goldenMajor(major: number): IGoldenRatio { // | void {
     // if (isNaN(major)) { return throwParaError(); }
-
     let main = major * this.goldenRatio;
     let minor = main - major;
 
     return { main, major, minor };
   }
 
-  goldenMinor(minor: number): IGoldenRatio | void {
+  //  @param minor Represents the "minor" segment
+  goldenMinor(minor: number): IGoldenRatio { // | void {
     // if (isNaN(minor)) { return throwParaError(); }
-
     let major = minor * this.goldenRatio;
     let main = minor + major;
 
